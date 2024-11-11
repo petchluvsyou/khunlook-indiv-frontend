2b0f475cb30911114b6270b0e3eaa23738e026b1
petchluvsyou <petchluvsyou@outlook.com>
2024-11-12 00:17:51 +0700

    fix: Article.tsx

--- src/components/Article.tsx
+++ src/components/Article.tsx
@@ -1,11 +1,23 @@
 import InformationBox from "@/components/InformationBox";
 
-export default function ContentSection({ title, description, informationBoxes }) {
+type InformationBoxProps = {
+    imgSrc: string;
+    title: string;
+    targetPage: string;
+};
+
+type ContentSectionProps = {
+    title: string;
+    description: string;
+    informationBoxes: InformationBoxProps[];
+};
+
+export default function ContentSection({ title, description, informationBoxes }: ContentSectionProps) {
     return (
-        <div className="flex justify-center items-center relative z-0 flex flex-col p-12 bg-[#F8F8F8] gap-1 top-[64px] sm:top-[92px] w-full">
+        <div className="flex justify-center items-center relative z-0 flex-col p-12 bg-Bg gap-1 top-16 sm:top-24 w-full">
             <div className="text-center">
-                <h1 className="font-bold text-[32px] sm:text-8xl mb-[6px] sm:mb-[12px]">{title}</h1>
-                <p className="flex justify-center items-center font-normal text-[16px] sm:text-[32px] mb-[6px] sm:mb-0 text-center">
+                <h1 className="font-bold text-3xl sm:text-8xl mb-1.5 sm:mb-3">{title}</h1>
+                <p className="flex justify-center items-center font-normal text-lg sm:text-3xl mb-1.5 sm:mb-0 text-center">
                     {description}
                 </p>
             </div>
