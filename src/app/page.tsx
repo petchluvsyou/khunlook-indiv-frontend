6633521a83f5053cd24297cc57b90ab5c6bd1830
petchluvsyou <petchluvsyou@outlook.com>
2024-11-11 22:13:13 +0700

    growth page (unfinished)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,13 @@ import Banner from "@/components/Banner";
 import FunctionCard from "@/components/FunctionCard";
 import DownloadCard from "@/components/DownloadCard";
 import Footer from "@/components/Footer";
+import StackedAreas from "@/components/stackedAreas";
 
 export default function Home() {
   return (
     <main>
       <Banner/>
+      <StackedAreas/>
       <FunctionCard/>
       <DownloadCard/>
     </main>
