cd9effb98d826ccff8cf17863d611bb1cfa3b222
Sirapatch Thammaleelakul <sirapetch@Siras-MacBook-Air.local>
2024-12-13 01:49:55 +0700

    b

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,23 +1,108 @@
-import Image from "next/image";
-import Link from "next/link";
+import Image from 'next/image';
+import Link from 'next/link';
 
-export default function Footer(){
-    return (
-        <div className="relative -z-20 flex flex-col p-12 pt-24 bg-Bg gap-12 text-center items-center">
-            <div className="grid grid-cols-3 lg:flex lg:flex-row justify-center gap-12 flex-wrap">
-                <Link href={'https://www.nrct.go.th/'}><Image src='/img/logo/NRCT.png' alt='NRCT logo' width={0} height={0} sizes='100vh' className='h-[120px] w-auto object-contain'/></Link>
-                <Link href={'https://www.hsri.or.th/'}><Image src='/img/logo/HSAI.png' alt='HSAI logo' width={0} height={0} sizes='100vh' className='h-[120px] w-auto object-contain'/></Link>
-                <Link href={'https://www.kku.ac.th/'}><Image src='/img/logo/KKU.png' alt='KKU logo' width={0} height={0} sizes='100vh' className='h-[120px] w-auto object-contain'/></Link>
-                <Link href={'https://www.thaipediatrics.org/'}><Image src='/img/logo/RCPT.png' alt='RCPT logo' width={0} height={0} sizes='100vh' className='h-[120px] w-auto object-contain'/></Link>
-                <Link href={'https://www.moph.go.th/'}><Image src='/img/logo/MOPH.png' alt='MOPH logo' width={0} height={0} sizes='100vh' className='h-[120px] w-auto object-contain'/></Link>
-                <Link href={'https://www.cp.eng.chula.ac.th/'}><Image src='/img/logo/chula.png' alt='chula logo' width={0} height={0} sizes='100vh' className='h-[120px] w-auto object-contain'/></Link>
-                <Link href={'https://web.facebook.com/people/%E0%B8%AA%E0%B8%B4%E0%B9%88%E0%B8%87%E0%B9%80%E0%B8%A5%E0%B9%87%E0%B8%81%E0%B9%86%E0%B8%97%E0%B8%B5%E0%B9%88%E0%B8%AA%E0%B8%A3%E0%B9%89%E0%B8%B2%E0%B8%87%E0%B8%A5%E0%B8%B9%E0%B8%81/100083413563342/'}>
-                    <Image src='/img/logo/SLL-trans.png' alt='SLL trans logo' width={0} height={0} sizes='100vh' className='h-[120px] w-auto object-contain'/>
-                </Link>
-                <Link href={'https://www.thaihealth.or.th/'}><Image src='/img/logo/SSS-trans.png' alt='SSS-trans logo' width={0} height={0} sizes='100vh' className='h-[120px] w-auto object-contain'/></Link>
-                <Link href={'https://www.nstda.or.th/'}><Image src='/img/logo/NSTDA.png' alt='NSTDA logo' width={0} height={0} sizes='100vh' className='h-[120px] w-auto object-contain'/></Link>
-            </div>
-            <p className="font-bold text-sm lg:text-xl">สงวนลิขสิทธิ์ ตาม พ.ร.บ.ลิขสิทธิ์ พ.ศ. 2537 โดย มหาวิทยาลัยขอนแก่น</p>
-        </div>
-    );
+export default function Footer() {
+	return (
+		<div className="relative z-0 flex flex-col p-12 pt-24 bg-Bg gap-12 text-center items-center mt-20">
+			<div className="grid grid-cols-3 lg:flex lg:flex-row justify-center gap-12 flex-wrap">
+				<Link href={'https://www.nrct.go.th/'}>
+					<Image
+						src="/img/logo/NRCT.png"
+						alt="NRCT logo"
+						width={0}
+						height={0}
+						sizes="100vh"
+						className="h-[120px] w-auto object-contain"
+					/>
+				</Link>
+				<Link href={'https://www.hsri.or.th/'}>
+					<Image
+						src="/img/logo/HSAI.png"
+						alt="HSAI logo"
+						width={0}
+						height={0}
+						sizes="100vh"
+						className="h-[120px] w-auto object-contain"
+					/>
+				</Link>
+				<Link href={'https://www.kku.ac.th/'}>
+					<Image
+						src="/img/logo/KKU.png"
+						alt="KKU logo"
+						width={0}
+						height={0}
+						sizes="100vh"
+						className="h-[120px] w-auto object-contain"
+					/>
+				</Link>
+				<Link href={'https://www.thaipediatrics.org/'}>
+					<Image
+						src="/img/logo/RCPT.png"
+						alt="RCPT logo"
+						width={0}
+						height={0}
+						sizes="100vh"
+						className="h-[120px] w-auto object-contain"
+					/>
+				</Link>
+				<Link href={'https://www.moph.go.th/'}>
+					<Image
+						src="/img/logo/MOPH.png"
+						alt="MOPH logo"
+						width={0}
+						height={0}
+						sizes="100vh"
+						className="h-[120px] w-auto object-contain"
+					/>
+				</Link>
+				<Link href={'https://www.cp.eng.chula.ac.th/'}>
+					<Image
+						src="/img/logo/chula.png"
+						alt="chula logo"
+						width={0}
+						height={0}
+						sizes="100vh"
+						className="h-[120px] w-auto object-contain"
+					/>
+				</Link>
+				<Link
+					href={
+						'https://web.facebook.com/people/%E0%B8%AA%E0%B8%B4%E0%B9%88%E0%B8%87%E0%B9%80%E0%B8%A5%E0%B9%87%E0%B8%81%E0%B9%86%E0%B8%97%E0%B8%B5%E0%B9%88%E0%B8%AA%E0%B8%A3%E0%B9%89%E0%B8%B2%E0%B8%87%E0%B8%A5%E0%B8%B9%E0%B8%81/100083413563342/'
+					}
+				>
+					<Image
+						src="/img/logo/SLL-trans.png"
+						alt="SLL trans logo"
+						width={0}
+						height={0}
+						sizes="100vh"
+						className="h-[120px] w-auto object-contain"
+					/>
+				</Link>
+				<Link href={'https://www.thaihealth.or.th/'}>
+					<Image
+						src="/img/logo/SSS-trans.png"
+						alt="SSS-trans logo"
+						width={0}
+						height={0}
+						sizes="100vh"
+						className="h-[120px] w-auto object-contain"
+					/>
+				</Link>
+				<Link href={'https://www.nstda.or.th/'}>
+					<Image
+						src="/img/logo/NSTDA.png"
+						alt="NSTDA logo"
+						width={0}
+						height={0}
+						sizes="100vh"
+						className="h-[120px] w-auto object-contain"
+					/>
+				</Link>
+			</div>
+			<p className="font-bold text-sm lg:text-xl">
+				สงวนลิขสิทธิ์ ตาม พ.ร.บ.ลิขสิทธิ์ พ.ศ. 2537 โดย มหาวิทยาลัยขอนแก่น
+			</p>
+		</div>
+	);
 }
--- src/components/FunctionCard.tsx
+++ src/components/FunctionCard.tsx
@@ -1,16 +1,36 @@
-import FunctionItem from "./FunctionItem";
+import FunctionItem from './FunctionItem';
 
-export default function FunctionCard(){
-    return (
-        <div className="relative z-0 flex flex-col p-16 pt-8 bg-Bg gap-12 top-16 lg:top-24 items-center">
-            <p className="text-xl lg:text-4xl font-bold">เราช่วยอะไรได้บ้าง</p>
-            <div className="flex flex-col lg:flex-row gap-8">
-                <FunctionItem imgSrc="/img/knowledge.png" title="คลังความรู้" description="รวมข้อมูลวัคซีนพื้นฐาน และวัคซีนเสริมหรือทดแทน ที่ควรได้รับในแต่ละวัย เป็นตัวช่วยเตือนให้คุณพ่อคุณแม่ทราบว่ามีวัคซีนไหนที่ยังขาด หรือ ควรต้องเสริมอะไรบ้าง"/>
-                <FunctionItem imgSrc="/img/growth.png" title="การเจริญเติบโต" description="รวมข้อมูลวัคซีนพื้นฐาน และวัคซีนเสริมหรือทดแทน ที่ควรได้รับในแต่ละวัย เป็นตัวช่วยเตือนให้คุณพ่อคุณแม่ทราบว่ามีวัคซีนไหนที่ยังขาด หรือ ควรต้องเสริมอะไรบ้าง"/>
-                <FunctionItem imgSrc="/img/development.png" title="พัฒนาการ" description="รวมข้อมูลวัคซีนพื้นฐาน และวัคซีนเสริมหรือทดแทน ที่ควรได้รับในแต่ละวัย เป็นตัวช่วยเตือนให้คุณพ่อคุณแม่ทราบว่ามีวัคซีนไหนที่ยังขาด หรือ ควรต้องเสริมอะไรบ้าง"/>
-                <FunctionItem imgSrc="/img/vaccine.png" title="วัคซีน" description="รวมข้อมูลวัคซีนพื้นฐาน และวัคซีนเสริมหรือทดแทน ที่ควรได้รับในแต่ละวัย เป็นตัวช่วยเตือนให้คุณพ่อคุณแม่ทราบว่ามีวัคซีนไหนที่ยังขาด หรือ ควรต้องเสริมอะไรบ้าง"/>
-                <FunctionItem imgSrc="/img/memory.png" title="สิ่งเล็กๆที่สร้างลูก" description="รวมข้อมูลวัคซีนพื้นฐาน และวัคซีนเสริมหรือทดแทน ที่ควรได้รับในแต่ละวัย เป็นตัวช่วยเตือนให้คุณพ่อคุณแม่ทราบว่ามีวัคซีนไหนที่ยังขาด หรือ ควรต้องเสริมอะไรบ้าง"/>
-            </div>
-        </div>
-    );
+export default function FunctionCard() {
+	return (
+		<div className="relative z-0 flex flex-col p-16 pt-8 bg-Bg gap-12 top-16 lg:top-24 items-center">
+			<p className="text-xl lg:text-4xl font-bold">เราช่วยอะไรได้บ้าง</p>
+			<div className="flex flex-col lg:flex-row gap-8">
+				<FunctionItem
+					imgSrc="/img/knowledge.png"
+					title="คลังความรู้"
+					description="รวมข้อมูลวัคซีนพื้นฐาน และวัคซีนเสริมหรือทดแทน ที่ควรได้รับในแต่ละวัย เป็นตัวช่วยเตือนให้คุณพ่อคุณแม่ทราบว่ามีวัคซีนไหนที่ยังขาด หรือ ควรต้องเสริมอะไรบ้าง"
+				/>
+				<FunctionItem
+					imgSrc="/img/growth.png"
+					title="การเจริญเติบโต"
+					description="รวมข้อมูลวัคซีนพื้นฐาน และวัคซีนเสริมหรือทดแทน ที่ควรได้รับในแต่ละวัย เป็นตัวช่วยเตือนให้คุณพ่อคุณแม่ทราบว่ามีวัคซีนไหนที่ยังขาด หรือ ควรต้องเสริมอะไรบ้าง"
+				/>
+				<FunctionItem
+					imgSrc="/img/development.png"
+					title="พัฒนาการ"
+					description="รวมข้อมูลวัคซีนพื้นฐาน และวัคซีนเสริมหรือทดแทน ที่ควรได้รับในแต่ละวัย เป็นตัวช่วยเตือนให้คุณพ่อคุณแม่ทราบว่ามีวัคซีนไหนที่ยังขาด หรือ ควรต้องเสริมอะไรบ้าง"
+				/>
+				<FunctionItem
+					imgSrc="/img/vaccine.png"
+					title="วัคซีน"
+					description="รวมข้อมูลวัคซีนพื้นฐาน และวัคซีนเสริมหรือทดแทน ที่ควรได้รับในแต่ละวัย เป็นตัวช่วยเตือนให้คุณพ่อคุณแม่ทราบว่ามีวัคซีนไหนที่ยังขาด หรือ ควรต้องเสริมอะไรบ้าง"
+				/>
+				<FunctionItem
+					imgSrc="/img/memory.png"
+					title="สิ่งเล็กๆที่สร้างลูก"
+					description="รวมข้อมูลวัคซีนพื้นฐาน และวัคซีนเสริมหรือทดแทน ที่ควรได้รับในแต่ละวัย เป็นตัวช่วยเตือนให้คุณพ่อคุณแม่ทราบว่ามีวัคซีนไหนที่ยังขาด หรือ ควรต้องเสริมอะไรบ้าง"
+				/>
+			</div>
+		</div>
+	);
 }
