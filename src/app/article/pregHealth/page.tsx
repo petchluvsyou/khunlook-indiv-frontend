854f2aaf2702fc0e7db4dc27ea0d5de9bc69e89c
teecv <teecv@teecvs-MacBook-Air.local>
2024-12-13 02:45:43 +0700

    add images and implement all article page

--- src/app/article/pregHealth/page.tsx
+++ src/app/article/pregHealth/page.tsx
@@ -3,14 +3,14 @@ import ContentSection from "@/components/Article";
 
 export default function Preg() {
     const informationBoxes = [
-        { imgSrc: "/img/knowledge.png", title: "การตรวจคัดกรองขณะตั้งครรภ์", targetPage: "/article/articlePage/preg_diag" },
-        { imgSrc: "/img/knowledge.png", title: "การเปลี่ยนแปลงของน้ำหนักของคุณแม่ขณะตั้งครรภ์", targetPage: "/article/articlePage/preg_weight" },
-        { imgSrc: "/img/knowledge.png", title: "ปัจจัยที่อาจส่งผลต่อคุณลูกในครรภ์", targetPage: "/article/articlePage/preg_factor" },
-        { imgSrc: "/img/knowledge.png", title: "วัคซีนสำหรับหญิงตั้งครรภ์", targetPage: "/article/articlePage/preg_vaccine" },
-        { imgSrc: "/img/knowledge.png", title: "อาการที่พบได้บ่อยขณะตั้งครรภ์", targetPage: "/article/articlePage/preg_effect" },
-        { imgSrc: "/img/knowledge.png", title: "อาหารสำหรับคุณแม่ตั้งครรภ์", targetPage: "/article/articlePage/preg_food" },
-        { imgSrc: "/img/knowledge.png", title: "อาหารสำหรับคุณแม่แบ่งตามช่วงอายุครรภ์", targetPage: "/article/articlePage/preg_foodrange" },
-        { imgSrc: "/img/knowledge.png", title: "เตรียมพร้อมก่อนคลอด", targetPage: "/article/articlePage/preg_prepare" },
+        { imgSrc: "/img/article_img/health/diag.png", title: "การตรวจคัดกรองขณะตั้งครรภ์", targetPage: "/article/articlePage/preg_diag" },
+        { imgSrc: "/img/article_img/health/portrait-beautiful-sexy-pregnant-young-woman-vertical-portrait-beautiful-sexy-pregnant-young-woman-happy-healthy-243260758.jpg.webp", title: "การเปลี่ยนแปลงของน้ำหนักของคุณแม่ขณะตั้งครรภ์", targetPage: "/article/articlePage/preg_weight" },
+        { imgSrc: "/img/article_img/health/effect.webp", title: "ปัจจัยที่อาจส่งผลต่อคุณลูกในครรภ์", targetPage: "/article/articlePage/preg_factor" },
+        { imgSrc: "/img/article_img/health/RS10399_GAVI_2012_Peter-Rudden_TANZANIA_1334-1.webp", title: "วัคซีนสำหรับหญิงตั้งครรภ์", targetPage: "/article/articlePage/preg_vaccine" },
+        { imgSrc: "/img/article_img/health/Meta_When_To_Take_Pregnancy_Test_For_Accurate_Results_1500x1000_f790c7b767.jpg", title: "อาการที่พบได้บ่อยขณะตั้งครรภ์", targetPage: "/article/articlePage/preg_effect" },
+        { imgSrc: "/img/article_img/health/Best-Foods-to-Eat-During-Pregnancy.webp", title: "อาหารสำหรับคุณแม่ตั้งครรภ์", targetPage: "/article/articlePage/preg_food" },
+        { imgSrc: "/img/article_img/health/Pregnancy-Plate.jpg.webp", title: "อาหารสำหรับคุณแม่แบ่งตามช่วงอายุครรภ์", targetPage: "/article/articlePage/preg_foodrange" },
+        { imgSrc: "/img/article_img/health/188350166_m_normal_none.jpg", title: "เตรียมพร้อมก่อนคลอด", targetPage: "/article/articlePage/preg_prepare" },
     ];
 
     return (
