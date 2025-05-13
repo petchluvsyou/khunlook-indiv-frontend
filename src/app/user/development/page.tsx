3c3c98533fc00a634d2e6ff778d1b9ba8e7d57e3
Vorraphan Treepoonsap <vorraphant@gmail.com>
2025-05-14 03:44:56 +0700

    fix: null age is now in dspm

--- src/app/user/development/page.tsx
+++ src/app/user/development/page.tsx
@@ -38,7 +38,7 @@ export default function page() {
   const { user, accessToken } = useAuth();
   const childServiceClass = new ChildService(accessToken ?? undefined);
   const DevelopmentServiceClass = new DevelopmentService(
-    accessToken ?? undefined,
+    accessToken ?? undefined
   );
 
   const getMatchingSkills = (code: string): ICurrentSkills | undefined => {
@@ -47,19 +47,20 @@ export default function page() {
   const handleChildOptionChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
     setChildOption(e.target.value);
     const currentIndex = Object.values(allChildInfo).findIndex(
-      (item) => item.NAME === e.target.value,
+      (item) => item.NAME === e.target.value
     );
     setChildIndex(currentIndex);
-
+    console.log(allChildInfo[currentIndex]);
     if (
       allChildInfo[currentIndex].ASPHYXIA === "1" ||
-      allChildInfo[currentIndex].BWEIGHT < 2500
+      (allChildInfo[currentIndex].BWEIGHT &&
+        allChildInfo[currentIndex].BWEIGHT < 2500)
     ) {
       toggleOption("เด็กกลุ่มเสี่ยง");
     } else {
       toggleOption("เด็กปฐมวัย");
     }
-    console.log(currentIndex);
+    console.log(allChildInfo[currentIndex]);
     // onLoadWindow();
   };
   const setDevelopmentInfo = async () => {
@@ -69,7 +70,7 @@ export default function page() {
       childpid: allChildInfo[childIndex]?.PID ?? "",
       childbirth: dayjs(allChildInfo[childIndex]?.BIRTH).format("YYYY-MM-DD"),
       childcorrectedbirth: dayjs(allChildInfo[childIndex]?.BIRTH).format(
-        "YYYY-MM-DD",
+        "YYYY-MM-DD"
       ),
       loggedin: 1,
       tableName: "",
@@ -102,14 +103,14 @@ export default function page() {
   const saveDevelopmentCallBack = async (
     daterec: string,
     developmentcode: string,
-    isUpdate: number,
+    isUpdate: number
   ) => {
     const request: ISaveDevelopmentRequest = {
       childpid: allChildInfo[childIndex].PID,
       dateocc: dayjs(daterec).format("YYYY-MM-DD"),
       childbirth: dayjs(allChildInfo[childIndex].BIRTH).format("YYYY-MM-DD"),
       childcorrectedbirth: dayjs(allChildInfo[childIndex].BIRTH).format(
-        "YYYY-MM-DD",
+        "YYYY-MM-DD"
       ),
       devcode: developmentcode,
       isUpdate: isUpdate,
@@ -140,7 +141,7 @@ export default function page() {
     }
   };
   const getDefaultAgeRange = (
-    option: "เด็กปฐมวัย" | "เด็กกลุ่มเสี่ยง",
+    option: "เด็กปฐมวัย" | "เด็กกลุ่มเสี่ยง"
   ): string => {
     return option === "เด็กปฐมวัย" ? "0-1 เดือน" : "แรกเกิด";
   };
@@ -160,11 +161,14 @@ export default function page() {
   const getAllChildInfo = async () => {
     try {
       const response = await childServiceClass.getChildByID(
-        user?.PID ?? "0000",
+        user?.PID ?? "0000"
       );
       const childInfo = response.data.data;
       setAllChildInfo(childInfo);
-      if (childInfo[0]?.ASPHYXIA === "1" || childInfo[0]?.BWEIGHT < 2500) {
+      if (
+        childInfo[0]?.ASPHYXIA === "1" ||
+        (childInfo[0].BWEIGHT && childInfo[0].BWEIGHT < 2500)
+      ) {
         toggleOption("เด็กกลุ่มเสี่ยง");
       } else {
         toggleOption("เด็กปฐมวัย");
@@ -447,11 +451,11 @@ export default function page() {
                 <DevelopmentCheckCell
                   currentSkills={currentSkills}
                   prev_chosen={currentSkills?.some(
-                    (item) => item.CODE === row.CODE,
+                    (item) => item.CODE === row.CODE
                   )}
                   prev_reserveDate={
                     dayjs(getMatchingSkills(row.CODE)?.DATE_OCCURRED).format(
-                      "YYYY-MM-DD",
+                      "YYYY-MM-DD"
                     ) ?? "0000-00-00"
                   }
                   saveDevelopmentCallBack={saveDevelopmentCallBack}
